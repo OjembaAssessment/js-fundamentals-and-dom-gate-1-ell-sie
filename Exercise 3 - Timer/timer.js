64e48744053cea0d24405c0ad0f53042279b9216
ll-sie <e.ndiramiye@alustudent.com>
2023-07-24 10:48:55 +0200

    exercise 3 js

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,27 +1,67 @@
 // console.log("Exercise 3 - Timer");
 const time = document.querySelector('h2');
-let btnContainer = document.querySelector('btn')
-let btn1 = document.querySelector('.add')
-let btn2 = document.querySelector('.minus')
+const resetButton = document.querySelector('.reset');
+const startStopButton = document.querySelector('.start');
+const addButton = document.querySelector('.add');
+const minusButton = document.querySelector('.minus');
 let timeSecond = 32;
+let countDown ;
+time.innerHTML = timeSecond + 's';
 
-time.innerHTML = timeSecond;
+// function updateTimer() {
+//   time.innerHTML = timeSecond + 's';
+// }
 
-const countDown = setInterval (()=>{
+function startCountdown() {
+    countDown = setInterval(() => {
     timeSecond--;
-    time.innerHTML = timeSecond;
-    if(timeSecond <=0 || timeSecond <1){
-        clearInterval(countDown);
-        hidden();
+    time.innerHTML = timeSecond + 's';
+
+    if (timeSecond <= 0 || timeSecond <1) {
+      clearInterval(countDown);
+      hidden();
     }
-},1000)
+  }, 1000);
+}
 
-btn1.addEventListener('click', function(){
+function stopCountdown() {
+  clearInterval(countDown);
+}
+
+addButton.addEventListener('click', function () {
     timeSecond += 1;
-    time.innerHTML = timeSecond;
-})
+    time.innerHTML = timeSecond + 's';
+  }
+);
 
-btn2.addEventListener('click', function(){
+minusButton.addEventListener('click', function () {
     timeSecond -= 1;
-    time.innerHTML = timeSecond;
-})
+    time.innerHTML = timeSecond + 's';
+});
+
+resetButton.addEventListener('click', function resetCountdown() {
+    stopCountdown();
+    timeSecond = 32; // Set it back to the initial value (32 seconds)
+    time.innerHTML = timeSecond + 's';
+    startStopButton.textContent = 'START';
+    addButton.disabled = false;
+    minusButton.disabled = false;
+  });
+
+startStopButton.addEventListener('click', function () {
+  if (countDown) {
+    // Countdown is running, stop it
+    stopCountdown();
+    startStopButton.textContent = 'START';
+    addButton.disabled = false;
+    minusButton.disabled = false;
+    resetButton.disabled = false;
+  } else {
+    // Countdown is not running, start it
+    startCountdown();
+    startStopButton.textContent = 'STOP';
+    addButton.disabled = true;
+    minusButton.disabled = true;
+    resetButton.disabled = true;
+  }
+});
